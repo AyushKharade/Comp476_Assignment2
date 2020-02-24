8eb430006360e57b91d127c8f51ff9cc508a1cb8
AyushKharade <ayush.kharade999@gmail.com>
2020-02-24 14:17:34 -0500

    setting up pathfinding nodes

---
+++ Comp476-Assignment2/Assets/Scripts/Node.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // node stores
+    // position
+    // g cost
+    // h cost
+    // total cost
+    // bool isEndNode.
+
+
+    // variables
+    Vector3 position;
+    float gCost;
+    float hCost;
+    float totalcost;
+
+    bool isFinalNode;
+    bool isInitialNode;
+
+    List<GameObject> neighbours;
+
+
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    
+}
