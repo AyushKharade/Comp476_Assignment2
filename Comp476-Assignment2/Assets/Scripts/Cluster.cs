aa13142b353020e7276c1df81ff3b57bdeed1d32
AyushKharade <ayush.kharade999@gmail.com>
2020-02-27 15:11:36 -0500

    clusters working as nodes

--- Comp476-Assignment2/Assets/Scripts/Cluster.cs
+++ Comp476-Assignment2/Assets/Scripts/Cluster.cs
@@ -6,6 +6,6 @@ public class Cluster : MonoBehaviour
 {
     public string clusterName;
 
-    public List<GameObject> clusterElements=new List<GameObject>();
+    //public List<GameObject> clusterElements=new List<GameObject>();
     public List<GameObject> clusterExits=new List<GameObject>();
 }
