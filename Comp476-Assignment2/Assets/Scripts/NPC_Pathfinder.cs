3990a789f1b3cd7327ccc7235627d97d83bd6224
AyushKharade <ayush.kharade999@gmail.com>
2020-02-27 19:51:34 -0500

    added npc and started npc movement script

---
+++ Comp476-Assignment2/Assets/Scripts/NPC_Pathfinder.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Pathfinder : MonoBehaviour
+{
+    // has a referece to the A* object.
+    // uses this reference to request a path to a point gotten from decision making.
+    // common script for both npcs
+
+    public float mSpeed=4;
+
+    public enum type { Chaser, Runner};
+    public type NPCType;
+
+    public GameObject AStarRef;
+    Pathfinding AStarScript;
+
+    public GameObject AllNodesParent;
+
+    int pathTraverseCounter=0;
+    Transform currentTarget;
+    public Transform currentDestination;
+    public Transform closestNode;
+
+    //public List<Transform> followPath = new List<Transform>();
+    public List<Vector3> followPath = new List<Vector3>();
+    public List<int> numbers = new List<int>();
+
+    bool hasDestination;
+
+
+    void Start()
+    {
+
+        //Debug.Log("" + AStarScript.scriptfunctionaccessTest);
+
+        //AStarRef = GameObject.FindGameObjectWithTag("A*");
+        /*
+        if (AStarRef != null)
+            Debug.Log("AStarRef is not null");
+        //AStarScript = AStarRef.GetComponent<Pathfinding>();
+
+        int r = Random.Range(0, AllNodesParent.transform.childCount);
+        currentDestination = AllNodesParent.transform.GetChild(r);
+        Debug.Log("Destination Set: " + currentDestination.name);
+
+        closestNode = FindClosestNode().transform;
+        //followPath = AStarRef.GetComponent<Pathfinding>().ClusterPathFind(closestNode.gameObject, currentTarget.gameObject);
+        numbers = AStarScript.ReturnTest();
+        Debug.Log(AStarRef.GetComponent<Pathfinding>().scriptfunctionaccessTest);
+    */
+    }
+
+    
+
+    void Update()
+    {
+        /*
+        if (!hasDestination)
+        {
+            if (followPath != null)
+            {
+                hasDestination = true;
+                closestNode = FindClosestNode().transform;
+                followPath = AStarScript.ClusterPathFind(closestNode.gameObject, currentTarget.gameObject);
+
+                currentTarget = followPath[0];
+                Debug.Log("Received Path");
+            }
+            else
+            {
+                Debug.Log("Path not received.");
+            }
+        }
+        else
+        {
+            // one by one follow path
+            if (Vector3.Distance(currentTarget.position, transform.position) == 0 && pathTraverseCounter != followPath.Count)
+            {
+                pathTraverseCounter++;
+                currentTarget = followPath[pathTraverseCounter];
+
+
+                Vector3 moveDir = (currentTarget.position - transform.position).normalized;
+                transform.Translate(moveDir * mSpeed * Time.deltaTime);
+            }
+        }
+        */
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Debug.Log("" + AStarScript.scriptfunctionaccessTest);
+            //AStarRef = GameObject.FindGameObjectWithTag("A*");
+            //AStarScript = AStarRef.GetComponent<Pathfinding>();
+
+            int r = Random.Range(0, AllNodesParent.transform.childCount);
+            currentDestination = AllNodesParent.transform.GetChild(r);
+            Debug.Log("Destination Set: " + currentDestination.name);
+
+            closestNode = FindClosestNode().transform;
+            numbers = AStarRef.GetComponent<Pathfinding>().ReturnTest();
+            int n=AStarRef.GetComponent<Pathfinding>().ReturnTestJustNumber();
+            Debug.Log(""+n);
+            Debug.Log(AStarRef.GetComponent<Pathfinding>().scriptfunctionaccessTest);
+            followPath= AStarRef.GetComponent<Pathfinding>().ClusterPathFind(closestNode.gameObject, currentDestination.gameObject);
+
+        }
+    }
+
+    GameObject FindClosestNode()
+    {
+        //overlap sphere
+        Collider[] arr = Physics.OverlapSphere(transform.position,10f);
+        GameObject ClosestNode = null;
+        float closestDistance=float.MaxValue;
+        foreach (Collider col in arr)
+        {
+            if (col.tag == "Node")
+            {
+                if (Vector3.Distance(transform.position, col.transform.position) < closestDistance)
+                {
+                    Vector3 rayOutPos = transform.position;
+                    rayOutPos.y += 0.2f;
+                    Vector3 dir = (col.transform.position - transform.position).normalized;
+
+                    RaycastHit hitobj;
+                    Physics.Raycast(rayOutPos,dir, out hitobj);     // Make sure its visible
+                    //Debug.Log("Ray out towards "+col.name+" hit: "+hitobj.collider.name);
+
+                    if (hitobj.collider.tag=="Node" && hitobj.collider.name == col.name)
+                    {
+                        ClosestNode = col.gameObject;
+                        closestDistance = Vector3.Distance(transform.position, col.transform.position);
+                    }
+                    
+                }
+            }
+        }
+        return ClosestNode;
+        //Debug.Log("Closest Node: "+closestNode.transform.name);
+    }
+}
--- Comp476-Assignment2/Assets/Scripts/Pathfinding.cs
+++ Comp476-Assignment2/Assets/Scripts/Pathfinding.cs
@@ -26,6 +26,7 @@ public class Pathfinding : MonoBehaviour
     public GameObject currentSelectedText;
     GameObject selectedHighlight;
 
+    [Header("Node & Cluster Parent Ref")]
     public GameObject AllNodesParent;
     public GameObject AllClustersParent;
 
@@ -46,6 +47,14 @@ public class Pathfinding : MonoBehaviour
     [Header("Heuristics")]
     public bool clusterHeuristic;     // if enable, call a seperate function
 
+    [Header("NPC Scene")]
+    public bool NPCScene;
+    public bool hideClusters;
+    public bool hideNodes;
+
+    // test
+    public string scriptfunctionaccessTest = "hhaha yes lmao";
+
     void Start()
     {
         OpenSet = new List<GameObject>();
@@ -53,12 +62,17 @@ public class Pathfinding : MonoBehaviour
 
         //GameObject currentSelectedText=GameObject.FindGameObjectWithTag("CurrentSelected");
         InitStartCosts();
+
+        if (hideNodes)
+            HideNodes();
+        if (hideClusters)
+            HideClusters();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!startedPathfinding)
+        if(!startedPathfinding && !NPCScene)
             MouseInput();
 
         if (showingNeighbours)
@@ -162,7 +176,7 @@ public class Pathfinding : MonoBehaviour
     {
         if (clusterHeuristic)
         {
-            StartPathfindingCluster();
+            StartPathfindingCluster(false);
         }
         else
         {
@@ -172,7 +186,42 @@ public class Pathfinding : MonoBehaviour
     }
 
 
-    
+    //public List<Transform> ClusterPathFind(GameObject startNode, GameObject endNode)
+    public List<Vector3> ClusterPathFind(GameObject startNode, GameObject endNode)
+    {
+        List<Transform> npcPath = new List<Transform>();
+
+        List<Vector3> vectorPath = new List<Vector3>();
+
+        StartNode = startNode;
+        EndNode = endNode;
+        Debug.Log("set start and end nodes.");
+
+        npcPath=StartPathfindingCluster(true);
+        foreach (Transform T in npcPath)
+        {
+            vectorPath.Add(T.position);
+        }
+
+
+        //return npcPath;
+        return vectorPath;
+    }
+
+    public List<int> ReturnTest()
+    {
+        List<int> temp = new List<int>();
+        temp.Add(2);
+        temp.Add(6);
+        temp.Add(3);
+
+        return temp;
+    }
+
+    public int ReturnTestJustNumber()
+    {
+        return 3;
+    }
 
 
     // Pathfinding Function:
@@ -258,19 +307,20 @@ public class Pathfinding : MonoBehaviour
 
 
     // cluster heuristics:
-    void StartPathfindingCluster()
+    List<Transform> StartPathfindingCluster(bool returnPath)
     {
         // check if start node and end nodes are both in the same cluster, if so, do regular path finding.
         GameObject clusterStart = StartNode.GetComponent<Node>().cluster;
         GameObject clusterEnd = EndNode.GetComponent<Node>().cluster;
 
-        if (clusterStart.transform.name == clusterEnd.transform.name)
+        if (clusterStart.transform.name == clusterEnd.transform.name && !NPCScene)
         {
             //StartPathfindingRegular();
             Debug.Log("Path within the same cluster");
             List<Transform> regularPath = new List<Transform>();
             regularPath = StartPathfindingRegular(StartNode,EndNode);
             DrawPath(regularPath);
+            return null;
         }
         else
         {
@@ -312,35 +362,22 @@ public class Pathfinding : MonoBehaviour
 
 
             // we have all the nodes we need to traverse through, draw these
+            List<Transform> clusterTraversePath=new List<Transform>();
             for (int i=0; i< (traverseThroughNodes.Count - 1); i++)
             {
-                List<Transform> temp = StartPathfindingRegular(traverseThroughNodes[i],traverseThroughNodes[i+1]);
-                DrawPath(temp);
+                 clusterTraversePath= StartPathfindingRegular(traverseThroughNodes[i],traverseThroughNodes[i+1]);
+                DrawPath(clusterTraversePath);
             }
 
+            if (returnPath)
+                return clusterTraversePath;
+            else
+                return null;
+
 
         }
     }
-
-
-    // function not used
-    public void TracePath2()
-    {
-        List<Transform> path = new List<Transform>();
-        GameObject curNode = EndNode;
-
-        path.Add(EndNode.transform);
-        while (curNode.GetComponent<Node>().Parent != null)
-        {
-            path.Add(curNode.GetComponent<Node>().Parent.transform);
-            curNode = curNode.GetComponent<Node>().Parent;
-        }
-
-        path.Reverse();
-
-        // now draw
-        DrawPath(path);
-    }
+    
 
     void DrawPath(List<Transform> lst)
     {
@@ -393,6 +430,24 @@ public class Pathfinding : MonoBehaviour
                 nb.GetComponent<MeshRenderer>().material = YellowMat;
                 showingNeighbours = true;
             }
+        }
+    }
+
+    void HideNodes()
+    {
+        int count = AllNodesParent.transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            AllNodesParent.transform.GetChild(i).GetComponent<MeshRenderer>().enabled = false;
+        }
+    }
+
+    void HideClusters()
+    {
+        int count = AllClustersParent.transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            AllClustersParent.transform.GetChild(i).GetComponent<MeshRenderer>().enabled = false;
         }
     }
 }
